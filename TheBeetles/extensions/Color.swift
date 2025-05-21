47a92df0940589772af6ba9fb530900d27aedf10
Stefani Pecalevska <stefani.pecalevska@netcetera.com>
2025-05-21 19:02:55 +0200

    color update 2

--- TheBeetles/extensions/Color.swift
+++ TheBeetles/extensions/Color.swift
@@ -1,12 +1,6 @@
 import UIKit
 import SwiftUI
 
-extension Color {
-    static let appBlue = Color("appCustomBlue")
-    static let appLightGray = Color("appCustomLightGray")
-    static let appRed = Color("appCustomRed")
-}
-
 extension UIApplication {
     func endEditing() {
         sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
